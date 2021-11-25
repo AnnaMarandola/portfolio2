bd24c3f987bfd8b5ab84a8e57d78c6d4ccae9d43
AnnaMarandola <annamarandola888@gmail.com>
2021-11-25 16:33:39 +0100

    hotfix

--- src/components/services/Booster.js
+++ src/components/services/Booster.js
@@ -1,5 +1,7 @@
 import { withStyles } from "@mui/styles";
 import { Typography } from "@mui/material";
+import CTAButton from "../mui/CTAButton";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const styles = (theme) => ({
   root: {
@@ -8,23 +10,25 @@ const styles = (theme) => ({
     flexDirection: "column",
     justifyContent: "space-evenly",
     textAlign: "center",
-    height: "10rem",
-    width: "70%",
+    height: "18rem",
+    width: "80%",
     margin: "3rem 0rem",
     padding: "1rem",
     [theme.breakpoints.up("sm")]: {
       margin: "5rem 0rem",
-
-    }
+    },
   },
 });
 
 const Booster = ({ classes }) => {
   return (
     <div className={classes.root}>
-      <Typography variant="h5">Une idée? un projet? des questions?</Typography>
-      <Typography variant="h5">N'hesitez pas, demandez votre devis!</Typography>
+      <Typography variant="h5">Une idée ? un projet ? des questions ?</Typography>
+      <Typography variant="h5">N'hesitez pas, demandez votre devis !</Typography>
       <Typography variant="h5">[ gratuit ]</Typography>
+      <AnchorLink href="#contact" className={classes.anchorLink}>
+        <CTAButton>Contact</CTAButton>
+      </AnchorLink>
     </div>
   );
 };
